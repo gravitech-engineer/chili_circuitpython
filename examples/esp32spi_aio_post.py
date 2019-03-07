488e1bac07f686b4ba6694955ced218114333188
KittitatN <kittitat@gravitechthai.com>
2019-03-07 16:56:02 +0700

    Add Example

---
+++ examples/esp32spi_aio_post.py
@@ -0,0 +1,44 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+
+print("ESP32 SPI webclient test")
+
+# Get wifi details and more from a settings.py file
+try:
+    from esp32spi_settings import settings
+except ImportError:
+    print("WiFi settings are kept in esp32spi_settings.py, please add them there!")
+    raise
+
+esp32_cs = DigitalInOut(board.D9)
+esp32_ready = DigitalInOut(board.D10)
+esp32_reset = DigitalInOut(board.D5)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, settings, board.NEOPIXEL)
+
+counter = 0
+while True:
+    try:
+        print("Posting data...", end='')
+        data = counter
+        feed = 'test'
+        payload = {'value':data}
+        response = wifi.post(
+            "https://io.adafruit.com/api/v2/"+settings['aio_username']+"/feeds/"+feed+"/data",
+            json=payload,headers={bytes("X-AIO-KEY","utf-8"):bytes(settings['aio_key'],"utf-8")})
+        print(response.json())
+        response.close()
+        counter = counter + 1
+        print("OK")
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        continue
+    response = None
+    time.sleep(15)
---
+++ examples/esp32spi_cheerlights.py
@@ -0,0 +1,65 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+
+import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
+
+# Get wifi details and more from a settings.py file
+try:
+    from esp32spi_settings import settings
+except ImportError:
+    print("WiFi settings are kept in esp32spi_settings.py, please add them there!")
+    raise
+
+print("ESP32 SPI webclient test")
+
+DATA_SOURCE = "https://api.thingspeak.com/channels/1417/feeds.json?results=1"
+DATA_LOCATION = ["feeds", 0, "field2"]
+
+esp32_cs = DigitalInOut(board.D9)
+esp32_ready = DigitalInOut(board.D10)
+esp32_reset = DigitalInOut(board.D5)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, settings, board.NEOPIXEL)
+
+# neopixels
+pixels = neopixel.NeoPixel(board.A1, 16, brightness=0.3)
+pixels.fill(0)
+
+# we'll save the value in question
+last_value = value = None
+
+while True:
+    try:
+        print("Fetching json from", DATA_SOURCE)
+        response = wifi.get(DATA_SOURCE)
+        print(response.json())
+        value=response.json()
+        for key in DATA_LOCATION:
+            value = value[key]
+            print(value)
+        response.close()
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        continue
+
+    if not value:
+        continue
+    if last_value != value:
+        color = int(value[1:],16)
+        red = color >> 16 & 0xFF
+        green = color >> 8 & 0xFF
+        blue = color& 0xFF
+        gamma_corrected = fancy.gamma_adjust(fancy.CRGB(red, green, blue)).pack()
+
+        pixels.fill(gamma_corrected)
+        last_value = value
+    response = None
+    time.sleep(60)
